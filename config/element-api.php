8913053ff8a9a8b80ba96f35a56afeef67b99d53
Dante <r0841336@student.thomasmore.be>
2024-01-11 13:52:13 +0100

    final commit

--- config/element-api.php
+++ config/element-api.php
@@ -16,9 +16,9 @@ return [
                         'id' => $entry->id,
                        'productTitle' => $entry->productTitle,
                        'price' => $entry->price,
-                       'bannerImg' => UrlHelper::url($entry->productImg->one()->getUrl('productImg'), ['scheme' => 'http']),
-
-
+                       'bannerImg' => str_replace('https', 'http', UrlHelper::url($entry->productImg->one()->getUrl('productImg'))),
+                       'categorie' => $entry-> categories->all() ? implode(', ', $entry->categories->all()): [],
+                       'fullText' => $entry->fullText,
                     ];
                 },
             ];
@@ -37,12 +37,67 @@ return [
                         'productTitle' => $entry->productTitle,
                         'price' => $entry->price,
                         'fullText' => $entry->fullText,
+                        'bannerImg' => str_replace('https', 'http', UrlHelper::url($entry->productImg->one()->getUrl('productImg'))),
+                        'categorie' => $entry-> categories->all() ? implode(', ', $entry->categories->all()): [],
 
                     ];
                 },
             ];
         },
 
+        '/api/contact' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'contactPage'],
+                'cache' => false, 
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'id' => $entry->id,
+                        'richtext' => $entry->richText,
+                    ];
+                },
+            ];
+        },
+
+        '/api/blog' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'blog'],
+                'cache' => false, 
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'id' => $entry->id,
+                        'richtext' => $entry->richText,
+                    ];
+                },
+            ];
+        },
+
+        '/api/team' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'team'],
+                'cache' => false, 
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'id' => $entry->id,
+                        'bannerImg' => str_replace('https', 'http', UrlHelper::url($entry->bannerImg->one()->getUrl('productImg'))),
+                        'naam' => $entry -> naamTeamlid,
+                        'email' => $entry->emailAdresTeamlid,
+                        'omschrijving' => $entry -> korteEigenTekst,
+
+                    ];
+                },
+            ];
+        },
 
     ]
-];
+    
+
+    ];
+    
+
+
